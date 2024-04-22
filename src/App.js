3db3989367c22771c68c715370de964710f33fff
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-04-22 15:04:45 +0300

    get, find, query in jest / tested user events

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
+
 function App() {
+	const [data, setData] = useState(null);
+	const [toggle, setToggle] = useState(false);
+	const [value, setValue] = useState('');
+
+	useEffect(() => {
+		setTimeout(() => {
+			setData({});
+		}, 100);
+	}, []);
+
 	return (
 		<>
-			<div>link</div>
-			<button>button</button>
+			{data && <div style={{ color: 'red' }}>data</div>}
+			{toggle && <div>hidden el</div>}
+			<button onClick={() => setToggle(!toggle)}>click</button>
+
+			<input
+				value={value}
+				onChange={e => setValue(e.target.value)}
+				placeholder="input..."
+			/>
+			<label data-testid="label">{value}</label>
 		</>
 	);
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,63 @@
-import { render, screen } from '@testing-library/react';
+import { findByText, render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+// 	render(<App />);
+
+// 	const linkElement = screen.getByText(/link/i);
+// 	expect(linkElement).not.toBeInTheDocument();
+// 	// expect(linkElement).toMatchSnapshot();
+
+// 	// find используется для асинхронного поиска элемента
+// 	// const buttonElement2 = screen.findByRole('button');
+
+// 	// get чтобы получить со стопроцентной вероятностью какой-то элемент или тест упадет.
+// 	// const buttonElement = screen.getByRole('button');
+
+// 	// query используется для доказательства того, что какого-то элемента нету на странице. Если элемент не найден, то тест не падает и возвращается null.
+// 	// const buttonElement3 = screen.queryByRole('buttonnnnn');
+// 	// expect(buttonElement3).toBeNull();
+
+// 	// screen.debug();
+// });
+
+test('async get data', async () => {
 	render(<App />);
 
-	const linkElement = screen.getByText(/link/i);
-	expect(linkElement).toBeInTheDocument();
-	// expect(linkElement).toMatchSnapshot();
-
-	// find используется для асинхронного поиска элемента
-	// const buttonElement2 = screen.findByRole('button');
-
-	// get чтобы получить со стопроцентной вероятностью какой-то элемент или тест упадет.
-	// const buttonElement = screen.getByRole('button');
-
-	// query используется для доказательства того, что какого-то элемента нету на странице. Если элемент не найден, то тест не падает и возвращается null.
-	// const buttonElement3 = screen.queryByRole('buttonnnnn');
-	// expect(buttonElement3).toBeNull();
-
 	// screen.debug();
+	const data = await screen.findByText(/data/i);
+	expect(data).toBeInTheDocument();
+	expect(data).toHaveStyle({ color: 'red' });
+	// screen.debug();
+});
+
+test('toggle event', () => {
+	render(<App />);
+
+	const buttonEl = screen.getByText(/click/i);
+	expect(buttonEl).toBeInTheDocument();
+
+	expect(screen.queryByText(/hidden el/i)).toBeNull();
+
+	fireEvent.click(buttonEl);
+	expect(screen.getByText(/hidden el/i)).toBeInTheDocument();
+
+	fireEvent.click(buttonEl, 'click');
+	expect(screen.queryByText(/hidden el/i)).toBeNull();
+});
+
+test('input event', () => {
+	render(<App />);
+
+	const inputEl = screen.getByPlaceholderText(/input.../i);
+	expect(inputEl).toBeInTheDocument();
+
+	fireEvent.input(inputEl, {
+		target: {
+			value: '123123'
+		}
+	});
+
+	const labelEl = screen.getByTestId('label');
+	expect(labelEl).toContainHTML('123123');
 });
