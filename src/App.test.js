04ceefc03af7cd77acd7bd41c056df74eb030e5d
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-04-22 14:13:33 +0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,21 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+
+	const linkElement = screen.getByText(/link/i);
+	expect(linkElement).toBeInTheDocument();
+	// expect(linkElement).toMatchSnapshot();
+
+	// find используется для асинхронного поиска элемента
+	// const buttonElement2 = screen.findByRole('button');
+
+	// get чтобы получить со стопроцентной вероятностью какой-то элемент или тест упадет.
+	// const buttonElement = screen.getByRole('button');
+
+	// query используется для доказательства того, что какого-то элемента нету на странице. Если элемент не найден, то тест не падает и возвращается null.
+	// const buttonElement3 = screen.queryByRole('buttonnnnn');
+	// expect(buttonElement3).toBeNull();
+
+	// screen.debug();
 });
