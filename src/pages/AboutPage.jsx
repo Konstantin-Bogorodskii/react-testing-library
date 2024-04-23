0d5bc09d8783c1f7ded23196ffc52c0e08b57ae8
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-04-23 13:07:49 +0300

    created components for following pages: About,Error, Main

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+	return <div data-testid="about-page">ABOUT PAGE</div>;
+};
+
+export default AboutPage;
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+	return <div data-testid="main-page">MAIN PAGE</div>;
+};
+
+export default MainPage;
