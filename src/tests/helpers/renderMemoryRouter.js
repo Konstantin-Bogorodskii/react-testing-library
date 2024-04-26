1f56f1ac24fa55c99b4721f3662be6a56f8a3b4c
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-04-26 17:46:05 +0300

    fix++

--- src/tests/helpers/renderMemoryRouter.js
+++ src/tests/helpers/renderMemoryRouter.js
@@ -2,12 +2,19 @@ import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import routes from '../../router/routes';
 import { render } from '@testing-library/react';
 
+import { Provider } from 'react-redux';
+import store from '../../store/store';
+
 const renderMemoryRouter = (initialRoute = '/') => {
 	const memoryRouter = createMemoryRouter(routes, {
 		initialEntries: [initialRoute]
 	});
 
-	return render(<RouterProvider router={memoryRouter} />);
+	return render(
+		<Provider store={store}>
+			<RouterProvider router={memoryRouter} />
+		</Provider>
+	);
 };
 
 export default renderMemoryRouter;
