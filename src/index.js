ee0f964ccda17f208a92f267789a29f2c04f7822
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-04-26 17:22:40 +0300

    configure redux store and connect it to app / created simple counterSlice

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './router/AppRouter.jsx';
 
+import { Provider } from 'react-redux';
+import store from './store/store.js';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
-		<AppRouter />
+		<Provider store={store}>
+			<AppRouter />
+		</Provider>
 	</React.StrictMode>
 );
